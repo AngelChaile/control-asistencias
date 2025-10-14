5a923d055ed06fda1fead63ea79c4087267a31f1
Angel Alejandro <angelchaile90@gmail.com>
2025-10-14 13:07:19 -0300

    cambio de csv a xlsx

--- src/components/ExportCSV.js
+++ src/components/ExportCSV.js
@@ -2,7 +2,7 @@ import Papa from "papaparse";
 
 export function exportToCsv(filename, rows) {
   const csv = Papa.unparse(rows);
-  const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  const blob = new Blob([xlsx], { type: "text/xlsx;charset=utf-8;" });
   const link = document.createElement("a");
   const url = URL.createObjectURL(blob);
   link.setAttribute("href", url);
